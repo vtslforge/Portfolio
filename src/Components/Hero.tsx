8c41a3e9b176a2777eb0a97bea983f2b0b3db5cd
vatsalya singh <singhvatsalya16@gmail.com>
2026-01-06 20:45:12 +0530

    changes

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,28 +3,37 @@ const Hero = () => {
     <main className="min-h-screen w-full flex px-3 font-poppins">
       <section className="w-full flex-1 pt-20">
         <article className="h-full flex flex-col justify-around ">
-          <header className="space-y-6 py-2" >
-            <p className="px-3 p-2 bg-navBarBackground w-fit rounded-full text-sm">
-              Avilable for work
-            </p>
-            <h1>
-              Full Stack Developer in Progress Building modern, responsive web
-              apps
-            </h1>
-          </header>
-          <figure className="relative text-center">
-            <img src="Group1.png"></img>
-            <p className="absolute bottom-30 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[clamp(3.5rem,22vw,80rem)] leading-0">Vatsalya</p>
-          </figure>
-          <article className="space-y-6 py-2 border">
-            <p>
+          <header className="flex flex-col justify-evenly h-full w-full">
+            <article className="flex lg:hidden ">
+              <p className="text-2xl w-1/2">
+                Full Stack Developer in Progress Building modern, responsive web
+                apps
+              </p>
+            </article>
+            <figure className="relative text-center flex justify-center items-center h-[60%] lg:h-2/3">
+              <img className="" src="/awww.png"></img>
+              <p className="absolute bottom-9 md:bottom-30 leading-0 text-[clamp(3.5rem,22vw,80rem)]">
+                Vatsalya
+              </p>
+              <article className="lg:absolute top-10 hidden lg:flex">
+                <p className="text-2xl w-1/3">
+                  Full Stack Developer in Progress Building modern, responsive
+                  web apps
+                </p>
+                <div className="w-1/3"></div>
+                <p className="text-2xl w-1/3">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Pariatur veritatis vel saepe ad impedit quas doloribus illum
+                  beatae tenetur! Optio aperiam quasi deleniti
+                </p>
+              </article>
+            </figure>
+            <p className="text-2xl w-full pt-7 lg:hidden">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
               veritatis vel saepe ad impedit quas doloribus illum beatae
-              tenetur! Optio aperiam quasi deleniti officia architecto
-              cupiditate incidunt, delectus suscipit maiores.
+              tenetur! Optio aperiam quasi deleniti
             </p>
-            <button className="border rounded-full p-3 px-6">Hire Me</button>
-          </article>
+          </header>
         </article>
       </section>
     </main>
