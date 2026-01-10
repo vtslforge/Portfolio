0121352a2b1f08fba5d173a1f097585dd1f3cbf2
vatsalya singh <singhvatsalya16@gmail.com>
2026-01-10 22:07:23 +0530

    imp

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,43 +1,26 @@
+
 const Hero = () => {
   return (
-    <main className="min-h-screen w-full flex px-3 font-poppins">
-      <section className="w-full flex-1 pt-20">
-        <article className="h-full flex flex-col justify-around ">
-          <header className="flex flex-col justify-evenly h-full w-full">
-            <article className="flex lg:hidden ">
-              <p className="text-2xl w-1/2">
-                Full Stack Developer in Progress Building modern, responsive web
-                apps
-              </p>
-            </article>
-            <figure className="relative text-center flex justify-center items-center h-[60%] lg:h-2/3">
-              <img className="" src="/awww.png"></img>
-              <p className="absolute bottom-9 md:bottom-30 leading-0 text-[clamp(3.5rem,22vw,80rem)]">
-                Vatsalya
-              </p>
-              <article className="lg:absolute top-10 hidden lg:flex">
-                <p className="text-2xl w-1/3">
-                  Full Stack Developer in Progress Building modern, responsive
-                  web apps
-                </p>
-                <div className="w-1/3"></div>
-                <p className="text-2xl w-1/3">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Pariatur veritatis vel saepe ad impedit quas doloribus illum
-                  beatae tenetur! Optio aperiam quasi deleniti
-                </p>
-              </article>
-            </figure>
-            <p className="text-2xl w-full pt-7 lg:hidden">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-              veritatis vel saepe ad impedit quas doloribus illum beatae
-              tenetur! Optio aperiam quasi deleniti
-            </p>
-          </header>
-        </article>
-      </section>
+    <main className='min-h-screen pt-15 md:p-15 flex flex-col justify-evenly p-3 pb-0 2xl:items-center'>
+      <header className='flex h-2/3 flex-col md:flex-row 2xl:w-3/4 font-poppins'>
+        <section className='flex flex-col justify-center gap-6 md:gap-15 md:w-1/2'>
+        <h1 className="text-4xl md:text-5xl xl:text-7xl">Hi, I'm a <span className="text-[#00F5FF]">full-stack developer</span> in progress</h1>
+        <h1 className="text-lg xl:text-2xl">I care deeply about scalability, performance, and SEO, because great products should grow fast and stay visible</h1>
+        <button className="p-2 text-sm w-20 md:w-30 cursor-pointer h-10 text-black flex justify-center items-center rounded-full bg-[#00F5FF] hover:bg-transparent hover:border hover:border-[#00F5FF] hover:text-white">contact</button>
+        </section>
+        <section className="md:w-1/2 flex justify-center items-center">
+        <img className="aspect-square" src='/awww.png' ></img>
+        </section>
+      </header>
+      <article className='h-1/3 flex justify-center items-center'>
+      <section className="absolute text-center w-fit text-lg p-3 md:p-6 rounded-2xl border">
+        <p className="text-sm font-poppins">With great code comes great console warnings💪</p>
+        <span className="absolute -top-6 h-10 bg-black text-7xl left-5 rounded-full font-mono">"</span>
+   
+        </section>
+      </article>
     </main>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,22 +1,40 @@
-import { RiDownloadFill } from "@remixicon/react";
 
 // import { NavLink } from "react-router-dom";
+import { useState , useEffect } from "react";
+import { RiMenu4Fill } from "@remixicon/react";
 
 const Navbar = () => {
+  const [open , setOpen] = useState(false);
+  useEffect(() => {
+  const onResize = () => window.innerWidth >= 768 && setOpen(false);
+  window.addEventListener("resize", onResize);
+  return () => window.removeEventListener("resize", onResize);
+}, []);
   return (
-    <div className="bg-navBarBackground border border-navbarBorderColor w-2/3 top-0 fixed h-12 mt-5 ml-3 rounded-full flex justify-end font-poppins ">
-      {/* <div className="w-1/5 justify-center flex items-center font-medium text-lg" ><p>Vatsalya Singh</p></div> */}
-      <div className="w-4/5 flex justify-center space-x-10 items-center text-fadeBlack " >
-      {/* <NavLink end to={"/"} className="hover:text-black" >Home</NavLink>
-      <NavLink end to={"/"} className="hover:text-black" >Projects</NavLink>
-      <NavLink end to={"/"} className="hover:text-black" >Blog</NavLink>
-      <NavLink end to={"/"} className="hover:text-black" >About me</NavLink>
-      <NavLink end to={"/"} className="hover:text-black" >Contact me</NavLink> */}
-      </div>
-      <div className="w-1/5 flex justify-center items-center" ><p className="cursor-pointer p-1 pl-4 pr-4 rounded-full text-fadeBlack hover:text-black flex items-center gap-2">Resume <RiDownloadFill className="size-4" /></p></div>
-
+    <div className="bg-[#0e0822] w-full z-50 top-0 fixed h-12 flex justify-between items-center font-poppins pl-3 pr-3">
+        <p className="font-poppins font-medium text-xl">Vatsalya singh</p>
+        <RiMenu4Fill onClick={()=>setOpen(!open)} className="relative md:hidden"/>
+          <section    className={`absolute top-12 right-3 bg-[#0e0822] border border-[#00F5FF] text-black w-50 h-80 rounded-3xl flex justify-center items-center transition-all duration-2s ease-in-out ${open? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"}`}>
+            <ul className="flex flex-col justify-between h-full p-3 items-center text-white"> 
+              <li className="cursor-pointer">Home</li>
+              <li className="cursor-pointer">Projects</li>
+              <li className="cursor-pointer">About me</li>
+              <li className="cursor-pointer">Blog</li>
+              <li className="cursor-pointer">contact</li>
+            </ul>
+          </section>
+          <section className="hidden md:flex h-full">
+            <ul className="flex h-full justify-center items-center gap-15">
+              <li className="cursor-pointer opacity-70 hover:opacity-100">Home</li>
+              <li className="cursor-pointer opacity-70 hover:opacity-100">Projects</li>
+              <li className="cursor-pointer opacity-70 hover:opacity-100">About me</li>
+              <li className="cursor-pointer opacity-70 hover:opacity-100">Blog</li>
+              <li className="cursor-pointer opacity-70 hover:opacity-100">contact</li>
+            </ul>
+          </section>
     </div>
   );
 };
 
 export default Navbar;
+
