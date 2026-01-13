0edf1a73a6f8369fa8f8c9013ef1fde381a39b19
vatsalya singh <singhvatsalya16@gmail.com>
2026-01-13 14:14:12 +0530

    nav rm fn

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,19 +1,19 @@
 
 // import { NavLink } from "react-router-dom";
-import { useState , useEffect } from "react";
-import { RiMenu4Fill } from "@remixicon/react";
+// import { useState , useEffect } from "react";
+// import { RiMenu4Fill } from "@remixicon/react";
 
 const Navbar = () => {
-  const [open , setOpen] = useState(false);
-  useEffect(() => {
-  const onResize = () => window.innerWidth >= 768 && setOpen(false);
-  window.addEventListener("resize", onResize);
-  return () => window.removeEventListener("resize", onResize);
-}, []);
+  // const [open , setOpen] = useState(false);
+  // useEffect(() => {
+  // const onResize = () => window.innerWidth >= 768 && setOpen(false);
+  // window.addEventListener("resize", onResize);
+  // return () => window.removeEventListener("resize", onResize);
+// }, []);
   return (
     <div className="bg-[#020015] w-full z-50 top-0 fixed h-12 flex justify-between items-center font-poppins pl-3 pr-3">
         <p className="font-poppins font-medium text-xl">Vatsalya singh</p>
-        <RiMenu4Fill onClick={()=>setOpen(!open)} className="relative md:hidden"/>
+        {/* <RiMenu4Fill onClick={()=>setOpen(!open)} className="relative md:hidden"/>
           <section    className={`absolute top-12 right-3 bg-[#0e0822] border border-[#00F5FF] text-black w-50 h-80 rounded-3xl flex justify-center items-center transition-all duration-2s ease-in-out ${open? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"}`}>
             <ul className="flex flex-col justify-between h-full p-3 items-center text-white"> 
               <a href="#home" className="cursor-pointer">home</a>
@@ -31,7 +31,7 @@ const Navbar = () => {
               <a href="#about" className="cursor-pointer opacity-70 hover:opacity-100">About me</a>
               <a href="#contact" className="cursor-pointer opacity-70 hover:opacity-100">contact</a>
             </ul>
-          </section>
+          </section> */}
     </div>
   );
 };
